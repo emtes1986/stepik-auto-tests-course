448e6d0901566d7c14c86b0b86cccb7e848e62e6
emtes1986 <76621367+emtes1986@users.noreply.github.com>
2021-01-11 11:48:26 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,19 +1,10 @@
-import pytest
 import time
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-    
-def test_guest_should_see_basket_link_on_page(browser):
+def test_page_has_add_to_basket_locale(browser):
     browser.get(link)
-    _step1 = browser.find_element_by_class_name("btn-add-to-basket")
-    time.sleep(30) 
-    try:
-        _step = browser.find_element_by_class_name("btn-add-to-basket")
-        return True
-    except NoSuchElementException:
-        print('Error: кнопка не найдена')
-        return False
+    #time.sleep(30)
+    has_button = browser.find_element_by_class_name('btn-add-to-basket')
+    assert has_button, 'кнопка добавления в корзину отсутствует'
     
