ae99123be17e78eeb714140bd3beac932fedf224
emtes1986 <76621367+emtes1986@users.noreply.github.com>
2021-01-11 10:47:23 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,11 +1,19 @@
 import pytest
+import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
+    
 def test_guest_should_see_basket_link_on_page(browser):
     browser.get(link)
+    _step1 = browser.find_element_by_class_name("btn-add-to-basket")
     time.sleep(30) 
-    basket = browser.find_element_by_css_selector(".basket-mini .btn-group > a")
-    assert basket, "кнопка не найдена"
+    try:
+        _step = browser.find_element_by_class_name("btn-add-to-basket")
+        return True
+    except NoSuchElementException:
+        print('Error: кнопка не найдена')
+        return False
+    
